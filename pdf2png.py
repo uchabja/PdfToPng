d70d3d7d6862905c9ac56021b7d6c6a32827dedd
uchabja <94525426+uchabja@users.noreply.github.com>
2023-05-15 19:39:29 +0300

    Create pdf2png.py

---
+++ pdf2png.py
@@ -0,0 +1,30 @@
+import os
+import fitz  # PyMuPDF
+
+# specify directory where the PDF files are
+pdf_dir = 'path/to/pdf/folder/'
+
+# specify directory where to save images
+img_dir = 'path/to/image/folder/'
+
+# ensure img_dir ends with '/'
+if not img_dir.endswith('/'):
+    img_dir += '/'
+
+# make img_dir if not exist
+os.makedirs(img_dir, exist_ok=True)
+
+# iterate over files in the pdf directory
+for filename in os.listdir(pdf_dir):
+    if filename.endswith('.pdf'):
+        # open pdf file
+        doc = fitz.open(pdf_dir + filename)
+        # iterate over pages in the pdf
+        for i in range(len(doc)):
+            # render page to an image (you can specify resolution here)
+            pix = doc[i].get_pixmap()
+            # save image as a PNG
+            img_file = img_dir + filename.rstrip('.pdf') + f'_page{i}.png'
+            pix.save(img_file)
+
+print("Conversion completed!")
